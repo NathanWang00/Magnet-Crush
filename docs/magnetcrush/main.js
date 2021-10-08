f13f3ec14053a41dd81a7f844755082f4cc4afbc
Nathan Wang <nxwang@ucsc.edu>
2021-10-07 22:46:05 -0700

    player movement

--- docs/magnetcrush/main.js
+++ docs/magnetcrush/main.js
@@ -1,14 +1,75 @@
-title = "";
+title = "Magnet Crush";
 
-description = `
+description = 
+`[Hold] 
+ Pull, Move
+ 
+[Release] 
+ Eject
 `;
 
-characters = [];
+characters = [
+`
+l    l 
+ llll 
+ l  l
+ llll
+ l  l
+`
+];
 
-options = {};
+const G = {
+	WIDTH: 110,
+	HEIGHT: 110,
+
+	PLAYER_MOVE_SPEED: 0.3,
+	PLAYER_PULL_RANGE: 25
+}
+
+options = {
+	viewSize: {x: G.WIDTH, y: G.HEIGHT}
+};
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * moveSpeed: number,
+ * velocity: Vector,
+ * isPulling: boolean
+ * }} Player
+ */
+
+/**
+ * @type { Player }
+ */
+let player;
 
 function update() {
+	//start
 	if (!ticks) {
-		
+		player = {
+            pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
+            moveSpeed: G.PLAYER_MOVE_SPEED,
+			velocity: vec(0, 0),
+            isPulling: false
+        };
 	}
+
+	//player
+	if (input.isPressed) {
+		if (player.pos.distanceTo(input.pos) > player.moveSpeed) {
+			player.velocity.x = player.moveSpeed * Math.cos(player.pos.angleTo(input.pos));
+			player.velocity.y = player.moveSpeed * Math.sin(player.pos.angleTo(input.pos));
+		} else {
+			player.velocity = vec(0, 0);
+		}
+			
+		
+	} else {
+		player.velocity.div(1.05);
+	}
+	player.pos.add(player.velocity);
+
+	color("black");
+	char("a", player.pos);
 }
